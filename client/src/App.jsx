3512710fc68aef5626965114983cf16a7e52612f
Davve420 <davidbrolin04@gmail.com>
2025-01-22 15:47:07 +0100

    layout started

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./Layout.jsx"
+import Home from "./pages/Home.jsx"
+
+export default function App(){
+  return<>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  </>
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+export default function Home(){
+    
+    return <>
+    <h1>PlushWorld</h1>
+    <div id="homeview">
+        <section>
+        <div id="PlushieView">
+            <p>Plushie</p>
+            <p>Plushie</p>
+            <p>Plushie</p>
+        </div>
+        </section>
+
+        <section>
+        <div id="PlushieCart">
+            <p>Plushie Cart</p>
+        </div>
+        </section>
+    </div>
+    </>
+}

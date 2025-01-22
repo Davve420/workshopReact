c7ca72518dae2c6b08777cd4aa594d09dd6b37ae
Patrik söderberg <patrik@dbi.nu>
2025-01-22 15:44:29 +0100

    dataset update

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+export default function app(){
+    const plushy = [
+        {
+        "id": "plush1-1",
+        "name": "Bluey",
+        "type": "Animal",
+        "color": "Blue",
+        "price": "150",
+        "image": "https://cdn.ty.com/static/images/eabf1ade4c7e7e726c29886e996bbc51_lg.png"
+        },
+        {
+            "id": "plush1-2",
+            "name": "Bingo",
+            "type": "Animal",
+            "color": "Brown",
+            "price": "150",
+            "image": "https://cdn.ty.com/static/images/a2c6048a331ce27e2f5330142407c8ef_lg.png"
+        },
+        {
+            "id": "plush1-3",
+            "name": "Dad",
+            "type": "Animal",
+            "color": "Blue",
+            "price": "150"
+            "image": "https://cdn.ty.com/static/images/48d02a9b2e97364dc3236e3da06a5637_lg.png"
+        }
+]
+}

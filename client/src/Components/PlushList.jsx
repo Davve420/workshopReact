28c70d4d463eb6c04d420fe049c8e2e71cc8fa73
Patrik <patrik@dbi.nu>
2025-01-23 14:13:27 +0100

    view

--- client/src/Components/PlushList.jsx
+++ client/src/Components/PlushList.jsx
@@ -1,7 +1,7 @@
 import Plushie from "./Plushie"
 
 export default function PlushieList({ listOfPlushies }) {
-    return <section id="cardList">
+    return <section id="plushView">
         {
             listOfPlushies.map(
                 (plushie, index) => <Plushie
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import data from "../public/data.js"
 import PlushieList from "../Components/PlushList.jsx"
 
 export default function Home() {
-    return <section className="page">
+    return <section className="plushList">
         <h1>PTC Seller</h1>
         <PlushieList listOfPlushies={data} />
     </section>
